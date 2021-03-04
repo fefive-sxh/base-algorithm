bb08f916ea45aa82cdcdb962741394061fe07282
fefive <13019718290@163.com>
2021-03-04 15:41:30 +0800

    :white_check_mark: 边界测试

--- sfOffer/find_in_matrix/findInMatrix_test.go
+++ sfOffer/find_in_matrix/findInMatrix_test.go
@@ -3,28 +3,32 @@ package find_in_matrix
 import "testing"
 
 type findMatrix struct {
-	matrix [][]int
-	target int
-	isIn   bool
+	matrix     [][]int
+	targetNums []int
+	isIn       bool
 }
 
 func createFindMatrix(t *testing.T, f *findMatrix) {
 	t.Helper()
-	if ok := FindInMatrix(f.matrix, f.target); f.isIn != ok {
-		t.Fatalf("the target: %d, the matrix: %v, expect: %v, got %v", f.target, f.matrix, f.isIn, ok)
+	for _, target := range f.targetNums {
+		if ok := FindInMatrix(f.matrix, target); f.isIn != ok {
+			// t.Error/t.Errorf 遇到错误不停, 继续执行其他测试用例
+			// t.Fatal/t.Fatalf 遇错即停
+			t.Errorf("the target: %d, the matrix: %v, expect: %v, got %v", target, f.matrix, f.isIn, ok)
+		}
 	}
 }
 
 func TestFindInMatrix(t *testing.T) {
 	createFindMatrix(t, &findMatrix{
-		matrix: [][]int{{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}},
-		target: 0,
-		isIn:   true,
+		matrix:     [][]int{{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}},
+		targetNums: []int{0, 3, 8, 11}, // 边界条件
+		isIn:       true,
 	})
 
 	createFindMatrix(t, &findMatrix{
-		matrix: [][]int{{1, 2, 3}, {4, 5, 7}, {8, 10, 12}},
-		target: 11,
-		isIn:   false,
+		matrix:     [][]int{{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}},
+		targetNums: []int{-1, 12}, // 错误条件
+		isIn:       false,
 	})
 }
